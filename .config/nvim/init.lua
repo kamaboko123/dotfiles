0ca5c4c399854142d74db31f3cf884fd8723d853
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2025-07-21 12:47:47 +0000

    nvim open with last cursor position

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,3 +14,13 @@ vim.o.autoindent = true
 -- Tab visualization
 vim.o.list = true
 vim.o.listchars = "tab:▸ ,trail:·,nbsp:␣,extends:>,precedes:<"
+
+-- Open with last cursor position
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
+      vim.cmd("normal! g`\"")
+    end
+  end,
+})
