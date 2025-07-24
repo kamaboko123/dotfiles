ae553a72623c7db5db8ea9875afc44913e3b9114
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2025-07-24 11:04:44 +0000

    change copilto keybind

--- .config/nvim/lua/copilot/init.lua
+++ .config/nvim/lua/copilot/init.lua
@@ -1,3 +1,14 @@
 return{
     "github/copilot.vim",
+    config = function()
+        vim.g.copilot_no_tab_map = true
+        vim.g.copilot_filetypes = {
+            ["*"] = true,
+        }
+        vim.api.nvim_set_keymap("i", "<M-l>", 'copilot#AcceptWord()', { expr = true, silent = true })
+        vim.api.nvim_set_keymap("i", "<M-L>", 'copilot#Accept()', { expr = true, silent = true })
+        vim.api.nvim_set_keymap("i", "<M-j>", 'copilot#Next()', { expr = true, silent = true })
+        vim.api.nvim_set_keymap("i", "<M-k>", 'copilot#Previous()', { expr = true, silent = true })
+        vim.api.nvim_set_keymap("i", "<M-h>", 'copilot#Dismiss()', { expr = true, silent = true })
+    end
 }
